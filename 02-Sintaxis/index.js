663787d57c83b2a2f9a797c79cefba7f8b3a44fd
teogiampietro <teogiampietro@gmail.com>
2022-12-03 20:00:01 -0300

    ejercicio 2

---
+++ 02-Sintaxis/index.js
@@ -0,0 +1,16 @@
+var lista = [
+        "Teo Giampietro",
+        25,
+        true,
+        "24/03/1997",
+        {
+            titulo: "Libro 1",
+            autor: "Autor 1" ,
+            fecha:  "01/01/2000",
+            url: 'https://www.esunaurl.com.ar'
+        }  
+];
+
+console.log(lista[0], lista[1], lista[2], lista[3]);
+
+console.log(lista[4].autor, lista[4].titulo, lista[4].fecha, lista[4].url);
